f63ae08d3a6b9ab3c94fc5f32508d743553b1a3c
abasinkanga <36732776+abasinkanga@users.noreply.github.com>
2018-06-05 23:04:58 +0100

    Update bot.js
    updated the api connection to steemit to api.steemit.com

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ var steem = require('steem');
 // URL taken from: https://developers.steem.io/
 // If server is unreliable, select another URL
 //		or run own node (2GB needed) as described in the linked docs
-steem.api.setOptions({ url: 'https://steemd.pevo.science/' });
+steem.api.setOptions({ url: 'https://api.steemit.com/' });
 
 var STEEMITURL = "https://steemit.com/";
 var LAST_TRANSACTION_FILEPATH = "./lastHandledTransaction.json";
